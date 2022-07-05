5b1643993eda235b9fa862464b8a1812b140f471
hee1231 <darong018@naver.com>
2022-07-05 23:06:57 +0900

    Feat : 로그인 폼 모듈 생성

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,19 +1,30 @@
-function Button ({href, size, label, active, primary, onClick}) {
-  const classList = ['button', size, primary ? '': 'secondary', active ? '' : 'disabled'];
+import styles from "./button.css";
+
+function Button({ href, size, label, active, primary, onClick }) {
+  const classList = [
+    "button",
+    size,
+    primary ? "" : "secondary",
+    active ? "" : "disabled",
+  ];
 
   return (
     <>
-    { href ? 
-      <a href={href} className={`${classList.join(' ')}`}>
-        {label}
-      </a> 
-     : 
-      <button type='button' onClick={onClick} className={`${classList.join(' ')}`}>
-        {label}
-      </button>
-    }
+      {href ? (
+        <a href={href} className={styles[`${classList.join(" ")}`]}>
+          {label}
+        </a>
+      ) : (
+        <button
+          type="button"
+          onClick={onClick}
+          className={`${classList.join(" ")}`}
+        >
+          {label}
+        </button>
+      )}
     </>
-  )
+  );
 }
 
 export default Button;
