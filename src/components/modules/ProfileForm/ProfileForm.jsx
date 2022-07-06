459bf092e4b5573ccaa717df6b90bb91867b44ca
DAHUI YEO <102221305+yeodahui@users.noreply.github.com>
2022-07-06 20:45:50 +0900

    Fix: ImageInput에 id props 전달

--- src/components/modules/ProfileForm/ProfileForm.jsx
+++ src/components/modules/ProfileForm/ProfileForm.jsx
@@ -5,7 +5,7 @@ import ImageInput from "../../atoms/ImageInput/ImageInput";
 function ProfileForm() {
   return (
     <div>
-      <ImageInput size="medium" a11y="프로필 이미지 파일 불러오기"/>
+      <ImageInput id="profileImageUpload" size="medium" a11y="프로필 이미지 파일 불러오기"/>
       <LoginInput id="userName" name="사용자 이름" type="text" />
       <LoginInput id="userId" name="계정 ID" type="text" />
       <LoginInput id="introduce" name="소개" type="text" />
