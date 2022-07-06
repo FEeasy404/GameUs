33014f33b1ba0727ff6f22e7244e2dba9a522d1e
hee1231 <darong018@naver.com>
2022-07-06 14:12:50 +0900

    Style: 프로필 설정 완료 버튼 상위 컴포넌트로 이동

--- src/components/modules/ProfileForm/ProfileForm.jsx
+++ src/components/modules/ProfileForm/ProfileForm.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import LoginInput from "../../atoms/LoginInput/LoginInput";
 import ProfileImageInput from "../../atoms/ProfileImageInput/ProfileImageInput";
-import Button from "../../atoms/Button/Button";
 
 function ProfileForm() {
   return (
@@ -10,12 +9,6 @@ function ProfileForm() {
       <LoginInput id="userName" name="사용자 이름" type="text" />
       <LoginInput id="userId" name="계정 ID" type="text" />
       <LoginInput id="introduce" name="소개" type="text" />
-      <Button
-        size="large"
-        label="게임어스 시작하기"
-        active={false}
-        primary={true}
-      />
     </div>
   );
 }
