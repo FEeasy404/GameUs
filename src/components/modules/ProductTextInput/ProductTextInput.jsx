c855400ba03789293fdf0cf665834681b28dae2f
Boram Kim <boram2445@gmail.com>
2022-07-06 17:22:19 +0900

    Feature: 상품 등록 페이지 하단 입력 폼 생성

---
+++ src/components/modules/ProductTextInput/ProductTextInput.jsx
@@ -0,0 +1,28 @@
+import InputBox from "../../atoms/InputBox/InputBox";
+
+function ProductTextInput() {
+  return (
+    <div>
+      <InputBox
+        id="product"
+        type="text"
+        name="상품명"
+        placeholder="2~5자 이내여야 합니다."
+      />
+      <InputBox
+        id="price"
+        type="text"
+        name="가격"
+        placeholder="숫자만 입력 가능합니다."
+      />
+      <InputBox
+        id="link"
+        type="text"
+        name="판매 링크"
+        placeholder="URL을 입력해주세요."
+      />
+    </div>
+  );
+}
+
+export default ProductTextInput;
