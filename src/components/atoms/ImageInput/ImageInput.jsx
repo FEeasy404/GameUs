b267fb56e8d84ce2f89dbe4d03330436c1e3ae56
yeodahui <yeodahui.dev@gmail.com>
2022-07-06 20:36:45 +0900

    Feat: 이미지 업로드 인풋 컴포넌트 생성

---
+++ src/components/atoms/ImageInput/ImageInput.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./imageinput.module.css"
+
+function ImageInput({a11y, size, id}) {
+  return (
+    <div>
+      <p className="a11y-hidden">{a11y}</p>
+      <label 
+      htmlFor={id}
+      className={[
+        styles["label-file"],
+        styles[size]
+        ].join(" ")}></label>
+      <input 
+        type="file" 
+        accept=".jpeg, .jpg, .png"
+        id={id}
+        className={"a11y-hidden"} 
+        // onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default ImageInput;
