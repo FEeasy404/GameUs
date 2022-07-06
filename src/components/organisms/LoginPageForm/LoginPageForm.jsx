0e3133bf18d9c59311f27ef91260edb726b793f1
donkeeman <doonkeeemaan@gmail.com>
2022-07-06 14:38:44 +0900

    Design: .max-width wrapper를 App.jsx에서 감싸주도록 변경

--- src/components/organisms/LoginPageForm/LoginPageForm.jsx
+++ src/components/organisms/LoginPageForm/LoginPageForm.jsx
@@ -4,10 +4,8 @@ import LoginForm from "../../modules/LoginForm/LoginForm";
 function LoginPageForm({ title, label }) {
   return (
     <section>
-      <div className="max-width">
         <h1>{title}</h1>
         <LoginForm label={label} />
-      </div>
     </section>
   );
 }
