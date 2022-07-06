650a7a16608af280565a6415fce80672f69a63da
Boram Kim <boram2445@gmail.com>
2022-07-06 21:45:03 +0900

    Fix: InputBox 컴포넌트명 미변경 파일 수정

--- src/components/modules/LoginForm/LoginForm.jsx
+++ src/components/modules/LoginForm/LoginForm.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import LoginInput from "../../atoms/LoginInput/LoginInput";
+import InputBox from "../../atoms/InputBox/InputBox";
 import Button from "../../atoms/Button/Button";
 
-function LoginForm({label}) {
+function LoginForm({ label }) {
   return (
     <div>
-      <LoginInput id="email" type="email" name="이메일" />
-      <LoginInput id="password" type="password" name="비밀번호" />
+      <InputBox id="email" type="email" name="이메일" />
+      <InputBox id="password" type="password" name="비밀번호" />
       <Button
         href={null}
         size="large"
