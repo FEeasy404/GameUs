21c38851a461f06a7b9a24b0b60a79abf2b856c8
yeodahui <yeodahui.dev@gmail.com>
2022-07-06 20:38:33 +0900

    Fix: ProfileForm 컴포넌트에서 ImageInput import 경로 수정

--- src/components/modules/ProfileForm/ProfileForm.jsx
+++ src/components/modules/ProfileForm/ProfileForm.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import LoginInput from "../../atoms/LoginInput/LoginInput";
-import ProfileImageInput from "../../atoms/ProfileImageInput/ProfileImageInput";
+import ImageInput from "../../atoms/ImageInput/ImageInput";
 
 function ProfileForm() {
   return (
     <div>
-      <ProfileImageInput />
+      <ImageInput size="medium" a11y="프로필 이미지 파일 불러오기"/>
       <LoginInput id="userName" name="사용자 이름" type="text" />
       <LoginInput id="userId" name="계정 ID" type="text" />
       <LoginInput id="introduce" name="소개" type="text" />
