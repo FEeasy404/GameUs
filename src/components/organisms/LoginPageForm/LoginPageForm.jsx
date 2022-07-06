c36ed177a6a86bb99e236926eee63ca290812e18
donkeeman <doonkeeemaan@gmail.com>
2022-07-06 11:14:12 +0900

    Style: LoginPageForm의 import문 따옴표 변경

--- src/components/organisms/LoginPageForm/LoginPageForm.jsx
+++ src/components/organisms/LoginPageForm/LoginPageForm.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 import LoginForm from "../../modules/LoginForm/LoginForm";
 
-function LoginPageForm({title, label}) {
+function LoginPageForm({ title, label }) {
   return (
     <section>
       <div className="max-width">
         <h1>{title}</h1>
-        <LoginForm label={label}/>
+        <LoginForm label={label} />
       </div>
     </section>
-  )
+  );
 }
 
-export default LoginPageForm
+export default LoginPageForm;
