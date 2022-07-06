717d6122da8c8c2df3f320117a7ba3c5dd012827
hee1231 <darong018@naver.com>
2022-07-06 14:16:55 +0900

    Feat: 프로필 설정 컴포넌트 생성

---
+++ src/components/organisms/ProfileSetting/ProfileSetting.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ProfileForm from "../../modules/ProfileForm/ProfileForm";
+import Button from "../../atoms/Button/Button";
+
+function ProfileSetting() {
+  return (
+    <div>
+      <h1>프로필 설정</h1>
+      <p>나중에 언제든지 변경할 수 있습니다.</p>
+      <ProfileForm />
+      <Button
+        size="large"
+        label="게임어스 시작하기"
+        active={false}
+        primary={true}
+      />
+    </div>
+  );
+}
+
+export default ProfileSetting;
