12c7fb29e476ac0618d331d065995c8417627297
yeodahui <yeodahui.dev@gmail.com>
2022-07-07 17:54:35 +0900

    Fix: 프로필 이미지와 버튼 순서 변경

--- src/components/modules/ProfileImageInput/ProfileImageInput.jsx
+++ src/components/modules/ProfileImageInput/ProfileImageInput.jsx
@@ -6,12 +6,12 @@ function ProfileImageInput() {
   const USER_PROFILE_IMG_URL = "https://www.nicepng.com/png/detail/129-1299268_pacman-ghost.png";
   return (
     <div>
+      <ImageBox src={USER_PROFILE_IMG_URL} type="circle" size="large" alt="사용자 프로필 사진"/>
       <ImageInput
         id="profileImageUpload"
         size="medium"
         a11y="프로필 이미지 파일 불러오기"
       />
-      <ImageBox src={USER_PROFILE_IMG_URL} type="circle" size="large" alt="사용자 프로필 사진"/>
     </div>
   )
 }
