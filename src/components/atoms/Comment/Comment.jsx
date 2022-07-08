acf98768c7494250e8d1520d86409d7bd0054f37
yeodahui <yeodahui.dev@gmail.com>
2022-07-08 20:36:14 +0900

    Style: 약어를 사용한 클래스명 수정

--- src/components/atoms/Comment/Comment.jsx
+++ src/components/atoms/Comment/Comment.jsx
@@ -3,7 +3,7 @@ import styles from "./comment.module.css"
 
 function Comment({postId, countComment}) {
   return (
-    <div className={styles["cont-comment"]}>
+    <div className={styles["container-comment"]}>
       <p className="a11y-hidden">댓글</p>
       <Link to={`/post/${postId}`} className={styles["link-comment"]} />
       <span className={styles["count-comment"]}>{countComment}</span>
