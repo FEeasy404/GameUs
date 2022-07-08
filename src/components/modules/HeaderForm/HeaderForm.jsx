c90dacdb1ce0a42d30cda80f87e03c4404c222d5
Boram Kim <boram2445@gmail.com>
2022-07-08 19:19:25 +0900

    Feat: 헤더 모듈 생성

---
+++ src/components/modules/HeaderForm/HeaderForm.jsx
@@ -0,0 +1,40 @@
+import HeaderInput from "../../atoms/HeaderInput/HeaderInput";
+import IconButton from "../../atoms/IconButton/IconButton";
+import Button from "../../atoms/Button/Button";
+import styles from "./headerForm.module.css";
+import { useNavigate } from "react-router-dom";
+
+function HeaderForm({
+  backButton,
+  searchButton,
+  menuButton,
+  title,
+  titleSize,
+  button,
+  Input,
+}) {
+  let navigate = useNavigate();
+  const classButton = button ? "button" : "";
+  return (
+    <header className={`${styles["header-wrapper"]} ${styles[classButton]}`}>
+      {backButton && (
+        <IconButton type="back" text="뒤로 가기" onClick={() => navigate(-1)} />
+      )}
+      {title && <h1 className={styles[titleSize]}>{title}</h1>}
+      {searchButton && <IconButton type="search" text="검색" onClick={""} />}
+      {menuButton && <IconButton type="menu" text="더보기" onClick={""} />}
+      {button && (
+        <Button
+          size="medium_small"
+          label={button}
+          active={true}
+          primary={true}
+          onClick={""}
+        />
+      )}
+      {Input && <HeaderInput placeholder="계정 검색" />}
+    </header>
+  );
+}
+
+export default HeaderForm;
