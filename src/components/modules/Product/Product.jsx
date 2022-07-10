323e977b6ed3d0e81531e1e2867634a09d9eea21
donkeeman <doonkeeemaan@gmail.com>
2022-07-10 11:43:28 +0900

    Comment: ImageBox 컴포넌트의 잘못 작성된 주석 수정

---
+++ src/components/modules/Product/Product.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ImageBox from "../../atoms/ImageBox/ImageBox";
+
+function Product() {
+  return (
+    <ImageBox type="rounded_square" src="https://www.nintendo.co.kr/software/switch/acbaa/set/img/img-box.png" alt="" />
+  );
+}
+
+export default Product;
