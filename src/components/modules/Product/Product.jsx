682f2659e0987cd9725fc05c605dcfc56b51364a
donkeeman <doonkeeemaan@gmail.com>
2022-07-10 13:06:05 +0900

    Feat: 판매 중인 상품 목록에 들어갈 상품 컴포넌트 및 css 구현

--- src/components/modules/Product/Product.jsx
+++ src/components/modules/Product/Product.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ImageBox from "../../atoms/ImageBox/ImageBox";
+import styles from "./product.module.css";
 
-function Product() {
+function Product({ src, name, price }) {
   return (
-    <ImageBox type="rounded_square" src="https://www.nintendo.co.kr/software/switch/acbaa/set/img/img-box.png" alt="" />
+    <div className={styles["wrapper-product"]}>
+      <ImageBox type="rounded_square" size="small" src={src} alt="" />
+      <span className={styles["product_name"]}>{name}</span>
+      <strong className={styles["product_price"]}>{price.toLocaleString("en-US")}원</strong>
+    </div>
   );
 }
 
