5ebcb79bb7f85b2acec6381b5b006eddac47cb6c
yeodahui <yeodahui.dev@gmail.com>
2022-07-11 11:01:34 +0900

    Feat: 포스트 카드 중 유저 정보 컴포넌트 생성

---
+++ src/components/atoms/Author/Author.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './author.module.css'
+
+function Author({userName, userId}) {
+  return (
+    <div>
+        <span className={styles["text-user_name"]}>{userName}</span>
+        <span className={styles["text-user_id"]}>{userId}</span>
+    </div>
+  )
+}
+
+export default Author
