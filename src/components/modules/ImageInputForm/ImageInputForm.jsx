a7743238e1dfa69278101b2823f41ee9b1de922d
Boram Kim <boram2445@gmail.com>
2022-07-15 20:56:04 +0900

    Refactor: 이미지 추가 컴포넌트에서 이미지를 상위 컴포넌트에서 받아오도록 변경

--- src/components/modules/ImageInputForm/ImageInputForm.jsx
+++ src/components/modules/ImageInputForm/ImageInputForm.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
 import ImageBox from "../../atoms/ImageBox/ImageBox";
 import ImageInputButton from "../../atoms/ImageInputButton/ImageInputButton";
 import styles from "./imageInputForm.module.css";
 
-function ImageInputForm({ a11y, boxType, boxSize, buttonColor }) {
+function ImageInputForm({
+  a11y,
+  boxType,
+  boxSize,
+  buttonColor,
+  saveImage,
+  image,
+}) {
   // 상위 컴포넌트로 이동 필요
-  const [image, setImage] = useState("");
-  function saveImage(event) {
-    setImage(URL.createObjectURL(event.target.files[0]));
-  }
+  // const [image, setImage] = useState("");
+  // function saveImage(event) {
+  //   setImage(URL.createObjectURL(event.target.files[0]));
+  //   console.log(image);
+  // }
   return (
     <div className={`${styles["image-input"]} ${styles[boxType]}`}>
       {a11y && <p className="a11y-hidden">{a11y}</p>}
