5214fd277a3a79a16fe3863b2c37a2b6700a4cef
donkeeman <doonkeeemaan@gmail.com>
2022-07-18 00:08:31 +0900

    Fix: 이메일 유효성 검사 정규식 수정

--- src/components/modules/LoginForm/LoginForm.jsx
+++ src/components/modules/LoginForm/LoginForm.jsx
@@ -47,7 +47,7 @@ function LoginForm({ label }) {
       const result = await data.json();
       console.log(result);
       const emailRegExp =
-        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+      /^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i;
       // 이메일 란이 비어있는 경우
       if (!email) {
         setEmailError("이메일을 입력해 주세요.");
