deb806bc1eec1bfc29a4caebc418def8d2a83a16
yeodahui <yeodahui.dev@gmail.com>
2022-07-18 17:17:30 +0900

    Feat: 루트 URL을 홈피드로 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import "./reset.css";
+import "./global.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import JoinPage from "./pages/JoinPage";
 import AddProductPage from "./pages/AddProductPage";
@@ -7,17 +9,21 @@ import ProfileEditPage from "./pages/ProfileEditPage";
 import UploadPage from "./pages/UploadPage/UploadPage";
 import FollowerPage from "./pages/FollowerPage";
 import FollowingPage from "./pages/FollowingPage.jsx";
-import "./reset.css";
-import "./global.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
+  //로그인 구현후 변경 필요
+  const accountname = "weniv_Gameland";
   return (
     <div className="max-width">
       <Routes>
-        <Route path="/" element={<h1>메인 페이지입니다.</h1>} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user/login" element={<LoginPage />} />
         <Route path="/user" element={<JoinPage />} />
-        <Route path="/product" element={<AddProductPage />} />
+        <Route
+          path="/product"
+          element={<AddProductPage accountname={accountname} />}
+        />
         <Route path="/profile/:accountname" element={<ProfilePage />} />
         <Route
           path="/profile/:accountname/follower"
@@ -28,10 +34,7 @@ function App() {
           element={<FollowingPage />}
         />
         <Route path="profile/edit" element={<ProfileEditPage />} />
-        <Route
-          path="/post"
-          element={<UploadPage />}
-        />
+        <Route path="/post" element={<UploadPage />} />
       </Routes>
     </div>
   );
