8a760814ac9eefc01eb1be691b90651def74353d
yeodahui <yeodahui.dev@gmail.com>
2022-07-19 16:36:31 +0900

    Fix: 유저 프로필 클릭 시 이동하는 경로 수정

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,19 +1,42 @@
 import HeaderForm from "../../components/modules/HeaderForm/HeaderForm";
+import ImageBox from "../../components/atoms/ImageBox/ImageBox";
 import styles from "./searchPage.module.css";
+import { Link } from "react-router-dom";
+
+// 마크업까지 구현되었으며, 추후 props를 통해 검색 기능을 추가할 예정
 
 function SearchPage() {
   return (
     <>
       <HeaderForm backButton={true} input={true} />
       <ul className={styles["container-users"]}>
-        <li className={styles["user"]}>
-          애월읍 위니브 감귤농장
+        <li>
+          <Link to={"profile/jordi3"} className={styles["container-user"]}>
+            <ImageBox 
+              src={"https://mandarin.api.weniv.co.kr/1657961502180.png"} 
+              type={"circle"} 
+              size={"medium"} 
+              alt={"jordi3"} 
+            />
+            <div className={styles["info-user"]}>
+              <strong className={styles["username"]}>죠르디</strong>
+              <span className={styles["id"]}>jordi3</span>
+            </div>
+          </Link>
         </li>
-        <li className={styles["user"]}>
-          애월읍 위니브 감귤농장
-        </li>
-        <li className={styles["user"]}>
-          애월읍 위니브 감귤농장
+        <li>
+          <Link to={"/profile/jordi3"} className={styles["container-user"]}>
+            <ImageBox 
+              src={"https://mandarin.api.weniv.co.kr/1657961502180.png"} 
+              type={"circle"} 
+              size={"medium"} 
+              alt={"jordi3"} 
+            />
+            <div className={styles["info-user"]}>
+              <strong className={styles["username"]}>죠르디</strong>
+              <span className={styles["id"]}>jordi3</span>
+            </div>
+          </Link>
         </li>
       </ul>
     </>
