dbbefc1f33c16a462faaa0d6309bf403a5d4376c
yeodahui <yeodahui.dev@gmail.com>
2022-07-20 17:20:43 +0900

    Feat: 읽지 않은 채팅 표시

--- src/pages/ChatListPage/ChatListPage.jsx
+++ src/pages/ChatListPage/ChatListPage.jsx
@@ -9,11 +9,13 @@ function ChatListPage() {
     username: "지상 최강의 뉴비",
     message: "주인공 이름이 젤다인가요?",
     date: "2020.10.25",
+    read: false,
   };
   const item2 = {
     username: "애국자",
     message: "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세",
     date: "2020.10.25",
+    read: true,
   }
   const chatData = [item1, item2];
   console.log(chatData)
@@ -25,7 +27,7 @@ function ChatListPage() {
       <ul className={styles["list-chat"]}>
         {chatData.map((item, index) => (
           <li key={index} className={styles["item-chat"]}>
-            <div className={styles.left}>
+            <div className={`${styles.left} ${item.read ? "" : styles.unread}`}>
               <ImageBox type={"circle"} size={"medium_small"} />
             </div>
             <div className={styles.right}>
