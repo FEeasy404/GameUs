f3cb59949462149d36b0e41d115877918e5bf925
Boram Kim <boram2445@gmail.com>
2022-07-20 14:38:47 +0900

    Feat: 댓글 계정이름 클릭시 해당 계정의 프로필로 이동 기능 추가

--- src/components/modules/CommentForm/CommentForm.jsx
+++ src/components/modules/CommentForm/CommentForm.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./commentForm.module.css";
 import IconButton from "../../atoms/IconButton/IconButton";
 import ImageBox from "../../atoms/ImageBox/ImageBox";
 
-function CommentForm({ src, userName, createTime, text }) {
+function CommentForm({ src, accoutName, userName, createTime, text }) {
+  const navigate = useNavigate();
   function handleMoreButton() {
     console.log("더보기");
   }
@@ -10,7 +12,10 @@ function CommentForm({ src, userName, createTime, text }) {
     <div className={styles["wrapper-comment"]}>
       <div className={styles["wrapper-upper"]}>
         <ImageBox src={src} type="circle" size="small" alt="프로필 이미지" />
-        <div className={styles["comment_info"]}>
+        <div
+          className={styles["comment_info"]}
+          onClick={() => navigate(`/profile/${accoutName}`)}
+        >
           <strong>{userName}</strong>
           <small>{`· ${createTime}`}</small>
         </div>
