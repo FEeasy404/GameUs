720f34925495151c6790787b34f0d7da057dff67
Boram Kim <boram2445@gmail.com>
2022-07-20 23:28:26 +0900

    Design: postCard 이미지 imageBox 컴포넌 꽉 차도록 조정

--- src/components/atoms/ImageBox/ImageBox.jsx
+++ src/components/atoms/ImageBox/ImageBox.jsx
@@ -4,7 +4,7 @@ import styles from "./imageBox.module.css";
 function ImageBox({ src, type, size, alt }) {
   return (
     <div className={`${styles["image"]} ${styles[type]} ${styles[size]}`}>
-      <img src={src} alt={alt} />
+      {src && <img src={src} alt={alt} />}
     </div>
   );
 }
