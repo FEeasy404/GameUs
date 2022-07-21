f5d4bb4af426ba713b06640aa741d4d2aeaa46dd
Boram Kim <boram2445@gmail.com>
2022-07-21 16:10:44 +0900

    Fix: postCard date 컴포넌트에 게시글 업로드 날짜가 나타나도록 변경

--- src/components/atoms/PostDate/PostDate.jsx
+++ src/components/atoms/PostDate/PostDate.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./postDate.module.css";
 
-function PostDate({date}) {
+function PostDate({ date }) {
   // 더미 데이터로 오늘 날짜가 찍히게 만들었습니다.
-  date = new Date();
+  date = new Date(date);
 
   // date를 xxxx년 x월 x일의 형태로 출력되게끔 string을 만듭니다.
   const month = date.getUTCMonth() + 1;
@@ -11,11 +11,7 @@ function PostDate({date}) {
   const year = date.getUTCFullYear();
   const DATE_STRING = `${year}년 ${month}월 ${day}일`;
 
-  return (
-    <span className={styles["date"]}>
-      {DATE_STRING}
-    </span>
-  )
+  return <span className={styles["date"]}>{DATE_STRING}</span>;
 }
 
 export default PostDate;
