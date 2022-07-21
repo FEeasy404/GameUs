37de82478568e8ba4ea879a5d41e8b2c2115361b
donkeeman <doonkeeemaan@gmail.com>
2022-07-21 19:10:14 +0900

    Refactor: 로그인, 회원가입의 state 통합 및 함수명 직관적으로 변경

--- src/components/modules/ProfileForm/ProfileForm.jsx
+++ src/components/modules/ProfileForm/ProfileForm.jsx
@@ -5,18 +5,10 @@ import styles from "./profileForm.module.css";
 
 function ProfileForm({
   setAccountnameValid,
-  image,
-  setImage,
-  username,
-  setUsername,
-  accountname,
-  setAccountname,
-  intro,
-  setIntro,
-  usernameError,
-  setUsernameError,
-  accountnameError,
-  setAccountnameError,
+  value,
+  setValue,
+  error,
+  setError,
   usernameInput,
   accountnameInput,
 }) {
@@ -26,57 +18,67 @@ function ProfileForm({
 
   // 사용자 이름과 계정 ID 내용이 바뀌면 에러가 표시되지 않도록 비웁니다.
   useEffect(() => {
-    setUsernameError("");
-  }, [username]);
+    setError({ ...error, username: "" });
+  }, [value.username]);
 
   useEffect(() => {
-    setAccountnameError("");
+    setError({ ...error, accountname: "" });
     setAccountnameValid(false);
-  }, [accountname]);
+  }, [value.accountname]);
 
   // 이미지를 선택하면 우선 로컬 url로 보여주는 함수입니다.
-  function handleImageInputData(event) {
+  function handleImageValue(event) {
     const file = event.target.files[0];
-    console.log(file);
     if (!file) {
       return;
     }
     const preview = { src: URL.createObjectURL(file), data: file };
-    setImage(preview);
+    setValue({ ...value, image: preview });
   }
 
   // 사용자 이름,계정 ID, 소개 셋 다 input이므로 한꺼번에 관리합니다.
-  function handleEditInputData(event) {
-    // input id가 "username" 이면 사용자 이름 세팅
-    if (event.target.id === "username") {
-      setUsername(event.target.value);
-      // input id가 "accountname" 이면 계정 ID 세팅
-    } else if (event.target.id === "accountname") {
-      setAccountname(event.target.value);
-      // input id가 "intro" 면 소개 세팅
-    } else if (event.target.id === "intro") {
-      setIntro(event.target.value);
-    }
+  function handleInputValue(event) {
+    setValue({ ...value, [event.target.id]: event.target.value });
   }
 
-  // username input이 blur되었을 때 유효성 검사를 실행하는 함수입니다.
-  function handleBlurUsername() {
+  function checkUsernameError() {
     // 사용자 이름 칸이 비어 있는지 검사합니다.
-    if (!username) {
-      setUsernameError("사용자 이름을 입력해 주세요.");
+    if (!value.username) {
+      setError({ ...error, username: "사용자 이름을 입력해 주세요." });
+      return false;
     }
     // 사용자 이름이 2~10자인지 검사합니다.
-    else if (username.length < 2 || username.length > 10) {
-      setUsernameError("사용자 이름은 2~10자 이내여야 합니다.");
+    else if (value.username.length < 2 || value.username.length > 10) {
+      setError({ ...error, username: "사용자 이름은 2~10자 이내여야 합니다." });
+      return false;
     }
+    return true;
+  }
+
+  function checkAccountnameError() {
+    // 계정 ID 칸이 비어 있는지 검사합니다.
+    if (!value.accountname) {
+      setError({ ...error, accountname: "계정 ID를 입력해 주세요." });
+      return false;
+    }
+    // 계정 ID 형식에 맞는지 검사합니다.
+    else if (!value.accountname.match(accountnameRegExp)) {
+      setError({
+        ...error,
+        accountname: "계정 ID는 영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.",
+      });
+      return false;
+    }
+    checkAccountnameValid();
+    return true;
   }
 
   // accountname input이 blur되었을 때 유효성 검사를 실행하는 함수입니다.
-  async function handleBlurAccountname() {
+  async function checkAccountnameValid() {
     // 이미 존재하는 계정 ID인지 검사합니다.
     const reqBody = {
       user: {
-        accountname: accountname,
+        accountname: value.accountname,
       },
     };
     try {
@@ -90,7 +92,7 @@ function ProfileForm({
       const result = await data.json();
 
       if (result.message == "이미 가입된 계정ID 입니다.") {
-        setAccountnameError(result.message);
+        setError({ ...error, accountname: result.message });
       }
       // 가입된 계정 ID가 아니라면 isAccountValid를 true로 바꿉니다.
       else {
@@ -98,16 +100,6 @@ function ProfileForm({
       }
     } catch (error) {
       console.log(error.message);
-    }
-    // 계정 ID 칸이 비어 있는지 검사합니다.
-    if (!accountname) {
-      setAccountnameError("계정 ID를 입력해 주세요.");
-    }
-    // 계정 ID 형식에 맞는지 검사합니다.
-    else if (!accountname.match(accountnameRegExp)) {
-      setAccountnameError(
-        "계정 ID는 영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-      );
     }
   }
 
@@ -118,21 +110,21 @@ function ProfileForm({
         boxType="circle"
         boxSize="large"
         image={
-          image
-            ? image.src
+          value.image
+            ? value.image.src
             : "https://mandarin.api.weniv.co.kr/1658306906297.png"
         }
-        saveImage={handleImageInputData}
+        saveImage={handleImageValue}
       />
       <InputBox
         id="username"
         name="사용자 이름"
         type="text"
         placeholder="2~10자 이내여야 합니다."
-        value={username}
-        error={usernameError}
-        onBlur={handleBlurUsername}
-        onChange={handleEditInputData}
+        value={value.username}
+        error={error.username}
+        onBlur={checkUsernameError}
+        onChange={handleInputValue}
         innerRef={usernameInput}
       />
       <InputBox
@@ -140,10 +132,10 @@ function ProfileForm({
         name="계정 ID"
         type="text"
         placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
-        value={accountname}
-        error={accountnameError}
-        onBlur={handleBlurAccountname}
-        onChange={handleEditInputData}
+        value={value.accountname}
+        error={error.accountname}
+        onBlur={checkAccountnameError}
+        onChange={handleInputValue}
         innerRef={accountnameInput}
       />
       <InputBox
@@ -151,8 +143,8 @@ function ProfileForm({
         name="소개"
         type="text"
         placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
-        value={intro}
-        onChange={handleEditInputData}
+        value={value.intro}
+        onChange={handleInputValue}
       />
     </div>
   );
