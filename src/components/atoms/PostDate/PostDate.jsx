a2176e3db0516f5e594429e0d6e7cca9a13fd6d2
Boram Kim <boram2445@gmail.com>
2022-07-21 16:42:19 +0900

    Style: 주석 삭제

--- src/components/atoms/PostDate/PostDate.jsx
+++ src/components/atoms/PostDate/PostDate.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./postDate.module.css";
 
 function PostDate({ date }) {
-  // 더미 데이터로 오늘 날짜가 찍히게 만들었습니다.
   date = new Date(date);
 
   // date를 xxxx년 x월 x일의 형태로 출력되게끔 string을 만듭니다.
