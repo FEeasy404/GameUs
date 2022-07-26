b8b015916c9909b8fb491c6c767416e215e906ec
yeodahui <yeodahui.dev@gmail.com>
2022-07-26 17:42:32 +0900

    Feat: ContextAPI 사용하여 현재 탭에 하이라이팅

--- src/components/atoms/NavItem/NavItem.jsx
+++ src/components/atoms/NavItem/NavItem.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { curruntPageContext } from "../../../contexts/curruntPageContext";
 import { Link } from "react-router-dom";
 import styles from "./navItem.module.css";
 
 function NavItem({link, label, icon}) {
-  const active = true;
+  // 현재 보고있는 탭에 하이라이팅
+  const { location } = useContext(curruntPageContext);
+  const { setLocation } = useContext(curruntPageContext);
+
+  let active = false;
+
+  if (location === icon) {
+    active = true;
+  }
+
   return (
-      <Link to={link} className={`${styles.link} ${styles[icon]} ${active ? styles.active : null}`}>
+      <Link 
+        to={link} 
+        onClick={setLocation(icon)}
+        className={`${styles.link} ${styles[icon]} ${active ? styles.active : null}`}
+      >
         <span>{label}</span>
       </Link>
   )
--- src/components/modules/BottomNavigateBar/BottomNavigateBar.jsx
+++ src/components/modules/BottomNavigateBar/BottomNavigateBar.jsx
@@ -1,28 +1,34 @@
+import { useState } from "react";
+import { curruntPageContext } from "../../../contexts/curruntPageContext";
 import NavItem from "../../atoms/NavItem/NavItem";
 import styles from "./bottomNavigateBar.module.css";
 
 function BottomNavigateBar() {
   const accountname = localStorage.getItem("accountname");
+  const [ location, setLocation ] = useState("home");
+
   return (
     <nav className={styles["nav"]}>
-      <ul className={styles["list-nav"]}>
-        <li className={`${styles["item-nav"]} ${styles["home"]}`}>
-          <NavItem link={"/"} label={"홈"} icon={"home"} />
-        </li>
-        <li className={`${styles["item-nav"]} ${styles["chat"]}`}>
-          <NavItem link={"/chat"} label={"채팅"} icon={"chat"} />
-        </li>
-        <li className={`${styles["item-nav"]} ${styles["post"]}`}>
-          <NavItem link={"/post"} label={"게시물 작성"} icon={"post"} />
-        </li>
-        <li className={`${styles["item-nav"]} ${styles["profile"]}`}>
-          <NavItem
-            link={`/profile/${accountname}`}
-            label={"프로필"}
-            icon={"profile"}
-          />
-        </li>
-      </ul>
+      <curruntPageContext.Provider value={{location, setLocation}}>
+        <ul className={styles["list-nav"]}>
+          <li className={`${styles["item-nav"]} ${styles["home"]}`}>
+            <NavItem link={"/"} label={"홈"} icon={"home"} />
+          </li>
+          <li className={`${styles["item-nav"]} ${styles["chat"]}`}>
+            <NavItem link={"/chat"} label={"채팅"} icon={"chat"} />
+          </li>
+          <li className={`${styles["item-nav"]} ${styles["post"]}`}>
+            <NavItem link={"/post"} label={"게시물 작성"} icon={"post"} />
+          </li>
+          <li className={`${styles["item-nav"]} ${styles["profile"]}`}>
+            <NavItem
+              link={`/profile/${accountname}`}
+              label={"프로필"}
+              icon={"profile"}
+            />
+          </li>
+        </ul>
+      </curruntPageContext.Provider>
     </nav>
   );
 }
