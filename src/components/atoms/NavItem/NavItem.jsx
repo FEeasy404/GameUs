82f1df9e97f0b3095f0cb35d63e3efd6017f730e
yeodahui <yeodahui.dev@gmail.com>
2022-07-27 09:50:10 +0900

    Fix: 현재 탭 하이라이팅을  path로 비교

--- src/components/atoms/NavItem/NavItem.jsx
+++ src/components/atoms/NavItem/NavItem.jsx
@@ -1,24 +1,13 @@
-import React, { useContext } from "react";
-import { curruntPageContext } from "../../../contexts/curruntPageContext";
 import { Link } from "react-router-dom";
 import styles from "./navItem.module.css";
 
-function NavItem({link, label, icon}) {
-  // 현재 보고있는 탭에 하이라이팅
-  const { location } = useContext(curruntPageContext);
-  const { setLocation } = useContext(curruntPageContext);
-
-  let active = false;
-
-  if (location === icon) {
-    active = true;
-  }
+function NavItem({link, label, icon}) { 
+  const currunt = location.pathname === link;
 
   return (
       <Link 
         to={link} 
-        onClick={setLocation(icon)}
-        className={`${styles.link} ${styles[icon]} ${active ? styles.active : null}`}
+        className={`${styles.link} ${styles[icon]} ${currunt ? styles.active : null}`}
       >
         <span>{label}</span>
       </Link>
