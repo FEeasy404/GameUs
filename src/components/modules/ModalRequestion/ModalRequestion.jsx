f1e98f76a00f35250722de029e27169fc8c437db
Boram Kim <boram2445@gmail.com>
2022-07-27 22:17:46 +0900

    Feat: 신고 모달 버튼 클릭시 재확인 모달 뜨도록 설정

--- src/components/modules/ModalRequestion/ModalRequestion.jsx
+++ src/components/modules/ModalRequestion/ModalRequestion.jsx
@@ -1,8 +1,9 @@
 import styles from "./modalRequestion.module.css";
 
-//로그아웃 또는 댓글, 게시글, 상품 삭제 재확인시 사용하는 모달
+//로그아웃 또는 댓글, 게시글, 상품 삭제, 신고 재확인시 사용하는 모달
 function ModalRequestion({ onClose, requestData }) {
   const name = requestData.name;
+  const text = requestData.text;
   return (
     <div
       className={styles["wrapper-modal"]}
@@ -11,7 +12,11 @@ function ModalRequestion({ onClose, requestData }) {
       }}
     >
       <strong>
-        {name === "로그인" ? `로그아웃 하시겠어요?` : `${name}을 삭제할까요?`}
+        {text === "신고"
+          ? "신고하시겠습니까?"
+          : name === "로그인"
+          ? `로그아웃 하시겠어요?`
+          : `${name}을 삭제할까요?`}
       </strong>
       <button onClick={onClose}>취소</button>
       <button
@@ -20,7 +25,7 @@ function ModalRequestion({ onClose, requestData }) {
           onClose();
         }}
       >
-        {name === "로그인" ? "로그아웃" : "삭제"}
+        {text === "신고" ? "신고" : name === "로그인" ? "로그아웃" : "삭제"}
       </button>
     </div>
   );
