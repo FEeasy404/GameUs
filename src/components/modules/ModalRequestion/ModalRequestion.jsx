a07fde7b722c1590386ae56f945c1069c35657c7
Boram Kim <boram2445@gmail.com>
2022-07-28 09:26:19 +0900

    Refactor: 모달의 복잡한 삼항연산자 계산 개선

--- src/components/modules/ModalRequestion/ModalRequestion.jsx
+++ src/components/modules/ModalRequestion/ModalRequestion.jsx
@@ -1,9 +1,19 @@
 import styles from "./modalRequestion.module.css";
 
 //로그아웃 또는 댓글, 게시글, 상품 삭제, 신고 재확인시 사용하는 모달
-function ModalRequestion({ onClose, requestData }) {
+function getModalText(requestData) {
   const name = requestData.name;
   const text = requestData.text;
+  if (text === "신고") {
+    return { modalText: "신고하시겠어요?", buttonText: "신고" };
+  } else if (name === "로그인") {
+    return { modalText: "로그아웃 하시겠어요?", buttonText: "로그아웃" };
+  } else {
+    return { modalText: `${name}을 삭제할까요?`, buttonText: "삭제" };
+  }
+}
+
+function ModalRequestion({ onClose, requestData }) {
   return (
     <div
       className={styles["wrapper-modal"]}
@@ -11,13 +21,7 @@ function ModalRequestion({ onClose, requestData }) {
         event.stopPropagation();
       }}
     >
-      <strong>
-        {text === "신고"
-          ? "신고하시겠습니까?"
-          : name === "로그인"
-          ? `로그아웃 하시겠어요?`
-          : `${name}을 삭제할까요?`}
-      </strong>
+      <strong>{getModalText(requestData).modalText}</strong>
       <button onClick={onClose}>취소</button>
       <button
         onClick={() => {
@@ -25,7 +29,7 @@ function ModalRequestion({ onClose, requestData }) {
           onClose();
         }}
       >
-        {text === "신고" ? "신고" : name === "로그인" ? "로그아웃" : "삭제"}
+        {getModalText(requestData).buttonText}
       </button>
     </div>
   );
