99594c7301914c8c189cf71672978ca2c3ba7c16
donkeeman <doonkeeemaan@gmail.com>
2022-07-29 19:27:56 +0900

    Fix: 상품의 가격이 너무 길 경우 말줄임되게 수정

--- src/components/modules/Product/Product.jsx
+++ src/components/modules/Product/Product.jsx
@@ -1,15 +1,38 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import ImageBox from "../../atoms/ImageBox/ImageBox";
 import styles from "./product.module.css";
 
 function Product({ product }) {
+  const price = useRef();
+  const [isOverflow, setOverflow] = useState(false);
+
+  useLayoutEffect(() => {
+    if (price.current.scrollWidth > price.current.clientWidth) {
+      setOverflow(true);
+    }
+  }, [price]);
+
   return (
     <div className={styles["wrapper-product"]}>
-      <ImageBox type="rounded_square" size="small" src={product.itemImage} alt="" />
-      <span className={styles["product_name"]}>{product.itemName}</span>
-      <strong className={styles["product_price"]}>
-        {product.price.toLocaleString("en-US")}원
+      <ImageBox
+        type="rounded_square"
+        size="small"
+        src={product.itemImage}
+        alt=""
+      />
+      <span className={styles["product_name"]} title={product.itemName}>
+        {product.itemName}
+      </span>
+      <strong
+        ref={price}
+        className={
+          isOverflow
+            ? styles["product_price_overflow"]
+            : styles["product_price"]
+        }
+        title={`${product.price.toLocaleString("en-US")}원`}
+      >
+        {product.price.toLocaleString("en-US")}
       </strong>
     </div>
   );
