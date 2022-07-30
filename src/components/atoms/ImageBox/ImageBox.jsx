b27929012414d6bf2ce788631089272d1012926f
Boram Kim <boram2445@gmail.com>
2022-07-30 22:00:26 +0900

    Feat: 이미지 경로 잘못되었을 경우 대체 이미지로 교체

--- src/components/atoms/ImageBox/ImageBox.jsx
+++ src/components/atoms/ImageBox/ImageBox.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import styles from "./imageBox.module.css";
 
 function ImageBox({ src, type, size, alt }) {
+  function handleError(e) {
+    e.target.src = "https://mandarin.api.weniv.co.kr/1659185236635.png";
+  }
   return (
     <div className={`${styles["image"]} ${styles[type]} ${styles[size]}`}>
-      {src && <img src={src} alt={alt} />}
+      {src && <img src={src} alt={alt} onError={handleError} />}
     </div>
   );
 }
