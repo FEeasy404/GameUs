a1cd05dd817409f12d18283354643b6293199af0
yeodahui <yeodahui.dev@gmail.com>
2022-07-31 17:20:39 +0900

    Feat: 상대방 채팅에 프로필 이미지 띄우도록 props 받아오기

--- src/components/modules/ChatRoom/ChatRoom.jsx
+++ src/components/modules/ChatRoom/ChatRoom.jsx
@@ -4,6 +4,9 @@ import UserChat from "../../atoms/Chat/UserChat/UserChat";
 import MyChat from "../../atoms/Chat/MyChat/MyChat";
 
 function ChatRoom() {
+  // 유저이미지 더미데이터입니다.
+  const image = "https://i.imgur.com/Cu85FEk.jpg";
+
   return (
     <div className={styles["wrapper-chatroom"]}>
       <UserChat
@@ -11,19 +14,21 @@ function ChatRoom() {
           "안녕하세요!! 저 이번에 처음 닌텐도 샀는데 궁금한 게 있어서 연락드려요"
         }
         date={"12:39"}
-      />
+        image={image}
+        />
       <UserChat
         text={"닌텐도 게임 자주 하시는 것 같아서요"}
         date={"12:41"}
-      />
+        image={image}
+        />
       <MyChat text={"아 네네 뉴비 환영이죠 ^^"} date={"12:50"} />
       <MyChat text={"어떤 게 궁금하세요?"} date={"12:50"} />
       <MyChat
         image="https://img.insight.co.kr/static/2020/01/30/700/j088zi88gyw66dup9kr2.jpg"
         date={"12:51"}
-      />
-      <UserChat text={"젤다의 전설"} date={"12:52"} />
-      <UserChat text={"주인공 이름이 젤다인가요?"} date={"12:52"} />
+        />
+      <UserChat text={"젤다의 전설"} date={"12:52"} image={image}/>
+      <UserChat text={"주인공 이름이 젤다인가요?"} date={"12:52"} image={image}/>
     </div>
   );
 }
