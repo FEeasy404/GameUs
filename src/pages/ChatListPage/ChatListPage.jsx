cea86ee81bbe2451c50f8ab94b0d98c3df8b71ac
yeodahui <yeodahui.dev@gmail.com>
2022-07-31 17:11:13 +0900

    Fix: 채팅목록 유저 프로필 이미지 추가 및 채팅방 링크 연결

--- src/pages/ChatListPage/ChatListPage.jsx
+++ src/pages/ChatListPage/ChatListPage.jsx
@@ -3,6 +3,7 @@ import ImageBox from "../../components/atoms/ImageBox/ImageBox";
 import HeaderForm from "../../components/modules/HeaderForm/HeaderForm";
 import BottomNavigateBar from "../../components/modules/BottomNavigateBar/BottomNavigateBar";
 import styles from "./chatListPage.module.css";
+import { Link } from "react-router-dom";
 
 function ChatListPage() {
   // 더미 데이터
@@ -11,12 +12,14 @@ function ChatListPage() {
     message: "주인공 이름이 젤다인가요?",
     date: "2020.10.25",
     read: false,
+    image: "https://i.imgur.com/Cu85FEk.jpeg",
   };
   const item2 = {
     username: "애국자",
     message: "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세",
     date: "2020.10.25",
     read: true,
+    image: "https://i.imgur.com/OFpTHKK.jpg",
   };
   const chatData = [item1, item2];
 
@@ -29,14 +32,16 @@ function ChatListPage() {
           {chatData.map((item, index) => (
             <li key={index} className={styles["item-chat"]}>
               <div className={`${styles.left} ${item.read ? "" : styles.unread}`}>
-                <ImageBox type={"circle"} size={"medium_small"} />
+                <ImageBox type={"circle"} size={"medium_small"} src={item.image} />
               </div>
               <div className={styles.right}>
-                <strong className={styles.username}>{item.username}</strong>
-                <div className={styles["info-message"]}>
-                  <span className={styles.message}>{item.message}</span>
-                  <span className={styles.date}>{item.date}</span>
-                </div>
+                <Link to={"/chat/strongnewbi"} >
+                  <strong className={styles.username}>{item.username}</strong>
+                  <div className={styles["info-message"]}>
+                    <span className={styles.message}>{item.message}</span>
+                    <span className={styles.date}>{item.date}</span>
+                  </div>  
+                </Link>
               </div>
             </li>
           ))}
