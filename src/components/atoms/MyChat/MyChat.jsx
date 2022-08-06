dde7600b92a9bb400eb8e6fdfaf25d27786dd467
donkeeman <doonkeeemaan@gmail.com>
2022-08-06 17:46:47 +0900

    Rename: atoms/Chat 안에 있던 UserChat과 MyChat을 atoms 폴더로 경로 변경

--- src/components/atoms/MyChat/MyChat.jsx
+++ src/components/atoms/MyChat/MyChat.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./mychat.module.css";
-import ImageBox from "../../ImageBox/ImageBox";
+import ImageBox from "../ImageBox/ImageBox";
 
 function MyChat({ text, image, date }) {
   return (
