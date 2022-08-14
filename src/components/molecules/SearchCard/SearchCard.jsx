e6339782fdc6fdbbff9d7cff1451d7aa518c9fda
Boram Kim <boram2445@gmail.com>
2022-08-14 20:45:29 +0900

    Feat: 검색 페이지 검색 키워드 색상 넣기 기능 추가

--- src/components/molecules/SearchCard/SearchCard.jsx
+++ src/components/molecules/SearchCard/SearchCard.jsx
@@ -2,10 +2,19 @@ import { Link } from "react-router-dom";
 import styles from "./searchCard.module.css";
 import ImageBox from "../../atoms/ImageBox/ImageBox";
 
-function SearchCard({ user }) {
-  // function handlekeywordColor() {
-  //   console.log(keyword);
-  // }
+function SearchCard({ user, keyword }) {
+  function handlekeywordColor(userString) {
+    if (userString.includes(keyword)) {
+      return (
+        <>
+          {userString.split(keyword)[0]}
+          <span className={styles["word-colored"]}>{keyword}</span>
+          {userString.split(keyword)[1]}
+        </>
+      );
+    }
+    return userString;
+  }
   return (
     <>
       <Link
@@ -19,8 +28,12 @@ function SearchCard({ user }) {
           alt={user.accountname}
         />
         <div className={styles["info-user"]}>
-          <strong className={styles["username"]}>{user.username}</strong>
-          <span className={styles["id"]}>{user.accountname}</span>
+          <strong className={styles["username"]}>
+            {handlekeywordColor(user.username)}
+          </strong>
+          <span className={styles["id"]}>
+            {handlekeywordColor(user.accountname)}
+          </span>
         </div>
       </Link>
     </>
