19dc4542c27f4d392e7ee359e86c325510fda696
donkeeman <doonkeeemaan@gmail.com>
2022-08-15 19:25:33 +0900

    Feat: 좋아요 기능 추가

--- src/components/atoms/Like/Like.jsx
+++ src/components/atoms/Like/Like.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
+import { useContext } from "react";
+import { LoginedUserContext } from "../../../contexts/LoginedUserContext";
 import styles from "./like.module.css";
+import { heartPost, unheartPost } from "../../molecules/PostCard/PostCardAPI";
+// import { useEffect } from "react";
 
-function Like({hearted, heartCount}) {
+function Like({ postId, hearted, heartCount }) {
   const [like, setLike] = useState(hearted);
   const [countLike, setCountLike] = useState(heartCount);
-  
-  function handleSetLike(event) {
-    setLike(!like);
+  const { user } = useContext(LoginedUserContext);
+
+  async function handleSetLike() {
+    let result;
+    if (like) {
+      result = await unheartPost(user.token, postId);
+    } else {
+      result = await heartPost(user.token, postId);
+    }
+    setLike(result.post.hearted);
     setCountLike(countLike + (like ? -1 : 1));
-    event.target.classList.toggle(styles.liked);
   }
 
   return (
     <div className={styles["container-like"]}>
       <p className="a11y-hidden">좋아요</p>
-      <button type="button" onClick={handleSetLike} className={styles["button-like"]}></button>
+      <button
+        type="button"
+        onClick={handleSetLike}
+        className={`${styles["button-like"]} ${like && styles["liked"]}`}
+      ></button>
       <span className={styles["count-like"]}>{countLike}</span>
     </div>
-  )
+  );
 }
 
 export default Like;
